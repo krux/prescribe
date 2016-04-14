07900968bb60ba3b23d9afdb8260bdcee304698d
Seth Yates <seth.yates@krux.com>
2016-04-13 22:11:40 -0700

    Clean up config files

--- test/karma-coverage.config.babel.js
+++ test/karma-coverage.config.babel.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
-import * as path from 'path';
+import path from 'path';
 import isparta from 'isparta';
-import pkg from './package.json';
+import pkg from '../package.json';
 import baseConfig from './karma-base.config.babel.js';
 
 module.exports = (config) => {
@@ -20,7 +20,7 @@ module.exports = (config) => {
     },
 
     instrumenter: {
-      'src/**/*.js': 'isparta'
+      '../src/**/*.js': 'isparta'
     },
 
     instrumenterOptions: {
@@ -30,17 +30,15 @@ module.exports = (config) => {
       }
     },
 
-    dir: 'dist/test-reports/coverage/',
+    dir: '../dist/test-reports/coverage/',
 
     reporters: [
       {type: 'text-summary'},
-      {type: 'text'},
-      {type: 'cobertura', subdir: '.', file: 'coverage.xml'},
-      {type: 'lcov'}
+      {type: 'text'}
     ]
   };
 
-  baseConfig.reporters = ['mocha', 'coverage', 'coveralls'];
+  baseConfig.reporters = ['mocha', 'coverage'];
 
   config.set(baseConfig);
 };
