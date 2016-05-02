376019da4983c33f683a518113f921b1001f3194
jnewman <joshua.newman@krux.com>
2016-05-02 17:12:54 -0500

    Fix nit

--- src/HtmlParser.js
+++ src/HtmlParser.js
@@ -99,7 +99,7 @@ export default class HtmlParser {
           const token = streamReaders[type](this.stream);
 
           if (token) {
-            if (token.type == 'startTag' &&
+            if (token.type === 'startTag' &&
                 (/script|style/i).test(token.tagName)) {
               return null;
             } else {
